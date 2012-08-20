c08d94c5912a0033e3a8a319803527010b19605b
Zaven Muradyan <zaven@kado.zanity.net>
2012-08-20 11:38:09 -0700

    Add events queueing
    Add queueing functions and storage to gtApplication

--- src/graviton.js
+++ src/graviton.js
@@ -183,6 +183,91 @@
      */
     var gtApplication = function(args) {
         var me = {
+            // Constants
+            //-----------------
+
+            keycodes: {
+                K_LEFT: 37,
+                K_UP: 38,
+                K_RIGHT: 39,
+                K_DOWN: 40,
+
+                K_0: 48,
+                K_1: 49,
+                K_2: 50,
+                K_3: 51,
+                K_4: 52,
+                K_5: 53,
+                K_6: 54,
+                K_7: 55,
+                K_8: 56,
+                K_9: 57,
+
+                K_A: 65,
+                K_B: 66,
+                K_C: 67,
+                K_D: 68,
+                K_E: 69,
+                K_F: 70,
+                K_G: 71,
+                K_H: 72,
+                K_I: 73,
+                K_J: 74,
+                K_K: 75,
+                K_L: 76,
+                K_M: 77,
+                K_N: 78,
+                K_O: 79,
+                K_P: 80,
+                K_Q: 81,
+                K_R: 82,
+                K_S: 83,
+                K_T: 84,
+                K_U: 85,
+                K_V: 86,
+                K_W: 87,
+                K_X: 88,
+                K_Y: 89,
+                K_Z: 90,
+
+                K_KP1: 97,
+                K_KP2: 98,
+                K_KP3: 99,
+                K_KP4: 100,
+                K_KP5: 101,
+                K_KP6: 102,
+                K_KP7: 103,
+                K_KP8: 104,
+                K_KP9: 105,
+
+                K_BACKSPACE: 8,
+                K_TAB: 9,
+                K_ENTER: 13,
+                K_SHIFT: 16,
+                K_CTRL: 17,
+                K_ALT: 18,
+                K_ESC: 27,
+                K_SPACE: 32
+            },
+
+            mousecodes: {
+                M_LEFT: 0,
+                M_MIDDLE: 1,
+                M_RIGHT: 2
+            },
+
+            eventcodes: {
+                MOUSEDOWN: 1000,
+                MOUSEUP: 1001,
+                MOUSEMOVE: 1002,
+                MOUSEWHEEL: 1003,
+                CLICK: 1004,
+                DBLCLICK: 1005,
+
+                KEYDOWN: 1010,
+                KEYUP: 1011
+            },
+
             // Attributes
             //-----------------
 
@@ -190,7 +275,8 @@
             options: null,
             sim: null,
             grid: null,
-            interaction: {},
+            queue: [],
+            // interaction: {},
 
             // Functions
             //------------------
@@ -286,97 +372,227 @@
                 });
             },
 
+            _qadd: function(event) {
+                this.queue.push(event);
+            },
+
+            _qpoll: function() {
+                return this.queue.shift();
+            },
+
+            _qget: function() {
+                // Replacing the reference is faster than `splice()`
+                var ref = this.queue;
+                this.queue = [];
+                return ref;
+            },
+
+            _qclear: function() {
+                this.queue = [];
+            },
+
             _wireupEvents: function() {
                 // Grid mouse events
-                L.addEvent('mousedown', this.grid, L.bind(this._handleMouseDown, this));
-                L.addEvent('mousemove', this.grid, L.bind(this._handleMouseMove, this));
-                L.addEvent('mouseup', this.grid, L.bind(this._handleMouseUp, this));
+                L.addEvent('click', this.grid, L.bind(this._handleClick, this));
+                L.addEvent('dblclick', this.grid, L.bind(this._handleDblClick, this));
 
+                L.addEvent('mousedown', this.grid, L.bind(this._handleMouseDown, this));
+                L.addEvent('mouseup', this.grid, L.bind(this._handleMouseUp, this));
+                L.addEvent('mousemove', this.grid, L.bind(this._handleMouseMove, this));
+                L.addEvent('mousewheel', this.grid, L.bind(this._handleMouseWheel, this));
+                // Firefox-specific DOM scroll
+                L.addEvent('DOMMouseScroll', this.grid, L.bind(this._handleMouseWheel, this));
+
+                // Grid key events
                 L.addEvent('keydown', document, L.bind(this._handleKeyDown, this));
+                L.addEvent('keyup', document, L.bind(this._handleKeyUp, this));
+            },
+
+            _handleClick: function(event) {
+                this._qadd({
+                    type: this.eventcodes.CLICK,
+                    position: this._getPosition(event),
+                    button: event.button,
+                    shift: event.shiftKey,
+                    ctrl: event.ctrlKey,
+                    timestamp: event.timeStamp
+                });
+            },
+
+            _handleDblClick: function(event) {
+                log.write('Double click: ' + event.button, 'debug');
+
+                this._qadd({
+                    type: this.eventcodes.DBLCLICK,
+                    position: this._getPosition(event),
+                    button: event.button,
+                    shift: event.shiftKey,
+                    ctrl: event.ctrlKey,
+                    timestamp: event.timeStamp
+                });
             },
 
             _handleMouseDown: function(event) {
-                // Add flag to signal other events
-                this.interaction.started = true;
+                log.write('Mouse down: ' + event.button, 'debug');
 
-                // Calculate offset on the grid from clientX/Y, because
-                // some browsers don't have event.offsetX/Y
-                var eventX = event.clientX - this.grid.offsetLeft,
-                    eventY = event.clientY - this.grid.offsetTop;
-
-                this.interaction.body = this.sim.addNewBody({
-                    x: eventX,
-                    y: eventY
+                this._qadd({
+                    type: this.eventcodes.MOUSEDOWN,
+                    position: this._getPosition(event),
+                    button: event.button,
+                    shift: event.shiftKey,
+                    ctrl: event.ctrlKey,
+                    timestamp: event.timeStamp
                 });
 
-                this.interaction.previous = {
-                    x: eventX,
-                    y: eventY
-                };
-            },
+                // Add flag to signal other events
+                // this.interaction.started = true;
 
-            _handleMouseMove: function(event) {
-                if (this.interaction.started) {
-                    this._redrawVector({
-                        from: {
-                            x: this.interaction.body.x,
-                            y: this.interaction.body.y
-                        },
-                        to: {
-                            x: event.offsetX,
-                            y: event.offsetY
-                        }
-                    });
-                }
+                // this.interaction.body = this.sim.addNewBody({
+                    // x: eventX,
+                    // y: eventY
+                // });
+
+                // this.interaction.previous = {
+                    // x: eventX,
+                    // y: eventY
+                // };
             },
 
             _handleMouseUp: function(event) {
-                if (this.interaction.started) {
-                    this.interaction.started = false;
-                    
-                    var x = event.offsetX,
-                        y = event.offsetY,
-                        body = this.interaction.body;
+                log.write('Mouse up: ' + event.button, 'debug');
 
-                    body.velX = (x - body.x) * 0.0000001;
-                    body.velY = (y - body.y) * 0.0000001;
+                this._qadd({
+                    type: this.eventcodes.MOUSEUP,
+                    position: this._getPosition(event),
+                    button: event.button,
+                    shift: event.shiftKey,
+                    ctrl: event.ctrlKey,
+                    timestamp: event.timeStamp
+                });
+                // if (this.interaction.started) {
+                    // this.interaction.started = false;
 
-                    this.sim.redraw();
-                }
+                    // var x = event.offsetX,
+                        // y = event.offsetY,
+                        // body = this.interaction.body;
+
+                    // body.velX = (x - body.x) * 0.0000001;
+                    // body.velY = (y - body.y) * 0.0000001;
+
+                    // this.sim.redraw();
+                // }
+            },
+
+            _handleMouseMove: function(event) {
+                this._qadd({
+                    type: this.eventcodes.MOUSEMOVE,
+                    position: this._getPosition(event),
+                    timestamp: event.timeStamp
+                });
+
+                // if (this.interaction.started) {
+                    // this._redrawVector({
+                        // from: {
+                            // x: this.interaction.body.x,
+                            // y: this.interaction.body.y
+                        // },
+                        // to: {
+                            // x: event.offsetX,
+                            // y: event.offsetY
+                        // }
+                    // });
+                // }
+            },
+
+            _handleMouseWheel: function(event) {
+                // Account for discrepancies between Firefox and Webkit
+                var delta = event.wheelDelta ?
+                    (event.wheelDelta / 120) :
+                    (event.detail / -3);
+
+                log.write('Scroll delta: ' + delta, 'debug');
+
+                this._qadd({
+                    type: this.eventcodes.MOUSEWHEEL,
+                    position: this._getPosition(event),
+                    wheeldelta: delta,
+                    shift: event.shiftKey,
+                    ctrl: event.ctrlKey,
+                    timestamp: event.timeStamp
+                });
+
+                // Prevent the window from scrolling
+                event.preventDefault();
             },
 
             _handleKeyDown: function(event) {
-                switch (event.which) {
-                    // 'Enter'
-                    case 13:
-                        // Start or stop simulation
-                        this.sim.toggle();
-                        break;
+                // Account for browser discrepancies
+                var key = event.keyCode || event.which;
 
-                    // 'C'
-                    case 67:
-                        // Clear simulation
-                        this.sim.clear();
-                        break;
+                log.write('Key down: ' + key, 'debug');
 
-                    // 'P'
-                    case 80:
-                        // Toggle trails
-                        this.sim.graphics.toggleTrails();
-                        break;
+                this._qadd({
+                    type: this.eventcodes.KEYDOWN,
+                    keycode: key,
+                    shift: event.shiftKey,
+                    ctrl: event.ctrlKey,
+                    timestamp: event.timeStamp
+                });
+                // switch (event.which) {
+                    // // 'Enter'
+                    // case 13:
+                        // // Start or stop simulation
+                        // this.sim.toggle();
+                        // break;
 
-                    // 'R'
-                    case 82:
-                        // Generate random objects
-                        this._generateBodies(10, {randomColors: true});
-                        break;
+                    // // 'C'
+                    // case 67:
+                        // // Clear simulation
+                        // this.sim.clear();
+                        // break;
 
-                    // T for test
-                    case 84:
-                        this.sim.addNewBody({x: this.options.width / 2, y: this.options.height / 2, velX: 0, velY: 0, mass: 2000, radius: 50, color: '#5A5A5A'});
-                        this.sim.addNewBody({x: this.options.width - 400, y: this.options.height / 2, velX: 0, velY: 0.000025, mass: 1, radius: 5, color: '#787878'});
-                        break;
-                }
+                    // // 'P'
+                    // case 80:
+                        // // Toggle trails
+                        // this.sim.graphics.toggleTrails();
+                        // break;
+
+                    // // 'R'
+                    // case 82:
+                        // // Generate random objects
+                        // this._generateBodies(10, {randomColors: true});
+                        // break;
+
+                    // // T for test
+                    // case 84:
+                        // this.sim.addNewBody({x: this.options.width / 2, y: this.options.height / 2, velX: 0, velY: 0, mass: 2000, radius: 50, color: '#5A5A5A'});
+                        // this.sim.addNewBody({x: this.options.width - 400, y: this.options.height / 2, velX: 0, velY: 0.000025, mass: 1, radius: 5, color: '#787878'});
+                        // break;
+                // }
+            },
+
+            _handleKeyUp: function(event) {
+                // Account for browser discrepancies
+                var key = event.keyCode || event.which;
+
+                log.write('Key up: ' + key, 'debug');
+
+                this._qadd({
+                    type: this.eventcodes.KEYUP,
+                    keycode: key,
+                    shift: event.shiftKey,
+                    ctrl: event.ctrlKey,
+                    timestamp: event.timeStamp
+                });
+            },
+
+            _getPosition: function(event) {
+                // Calculate offset on the grid from clientX/Y, because
+                // some browsers don't have event.offsetX/Y
+                return {
+                    x: event.clientX - this.grid.offsetLeft,
+                    y: event.clientY - this.grid.offsetTop
+                };
             }
         };
 
@@ -554,7 +770,7 @@
                         if (r <= clearance) {
                             // Collision detected
                             if (this.options.logging)
-                                log.write('Collision detected!!');
+                                log.write('Collision detected!!', 'debug');
 
                         }
                     }
